2f9202e39315826a7b4cdff02bfa4f87e449ba63
Toby <toby.king77@gmail.com>
2023-06-14 20:30:11 +0100

    Designs + Flesch Graph + Pages

--- popup.js
+++ popup.js
@@ -1,4 +1,70 @@
-  // Get active browser tab
+// Menu Functions
+window.addEventListener("DOMContentLoaded", (event) => {
+  document.getElementById("showTextAnalysis").addEventListener("click", showTextAnalysis);
+  document.getElementById("showBias").addEventListener("click", showBias);
+});
+
+function showTextAnalysis() {
+  var x = document.getElementById("textAnalysisPage");
+  var y = document.getElementById("biasPage");
+  var i = document.getElementById("textAnalysis")
+  var j = document.getElementById("bias")
+
+  x.style.display = "block";
+  y.style.display = "none";
+  i.classList.add("active");
+  j.classList.remove("active");
+}
+
+function showBias() {
+  var x = document.getElementById("biasPage");
+  var y = document.getElementById("textAnalysisPage");
+  var i = document.getElementById("textAnalysis")
+  var j = document.getElementById("bias")
+
+  x.style.display = "block";
+  y.style.display = "none";
+  j.classList.add("active");
+  i.classList.remove("active");
+}
+
+// progressbar.js@1.0.0 version is used
+// Docs: http://progressbarjs.readthedocs.org/en/1.0.0/
+function displayFKScore(score) {
+  var bar = new ProgressBar.SemiCircle(container, {
+    strokeWidth: 6,
+    color: '#FFEA82',
+    trailColor: '#eee',
+    trailWidth: 1,
+    easing: 'easeInOut',
+    duration: 1400,
+    svgStyle: null,
+    text: {
+      value: '',
+      alignToBottom: false
+    },
+    from: {color: '#ED6A5A'},
+    to: {color: '#5aed6a'},
+    // Set default step function for all animate calls
+    step: (state, bar) => {
+      bar.path.setAttribute('stroke', state.color);
+      var value = (bar.value() * 100).toFixed(2);
+      if (value === 0) {
+        bar.setText('');
+      } else {
+        bar.setText(value);
+      }
+  
+      bar.text.style.color = '#000';
+    }
+  });
+  bar.text.style.fontFamily = '"Oswald", Helvetica, sans-serif';
+  bar.text.style.fontSize = '2rem';
+  
+  bar.animate(score);  // Number from 0.0 to 1.0
+}
+
+// Get active browser tab
 chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
   let url = tabs[0].url;
   // Inject content script into the active tab
@@ -23,18 +89,16 @@ chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
         // Calculate the average number of syllables per word
         const words = text.match(/\b\w+\b/g);
         const totalSyllables = words.reduce((total, word) => total + countSyllables(word), 0);
-        const averageSyllablesPerWord = totalSyllables / words.length;
       
         // Calculate the average number of words per sentence
         const sentences = text.match(/[.!?]+/g);
         const totalWords = words.length;
-        const averageWordsPerSentence = totalWords / sentences.length;
       
         // Calculate the Flesch-Kincaid readability score
         const readabilityScore =
-          0.39 * averageWordsPerSentence + 11.8 * averageSyllablesPerWord - 15.59;
+          206.835 - 1.015 * (totalWords / sentences.length) - 84.6 * (totalSyllables / totalWords);
       
-        return readabilityScore.toFixed(2); // Return the score rounded to 2 decimal places
+        return readabilityScore.toFixed(2) / 100; // Return the score rounded to 2 decimal places
       }
       
       // Helper function to count the number of syllables in a word
@@ -46,17 +110,17 @@ chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
         return syllables ? syllables.length : 0;
       }
 
-
-      document.getElementById("url").innerHTML = url;
+      //document.getElementById("url").innerHTML = url;
       if (results[0].result.trim().split(/\s+/).length <= 1) {
-        document.getElementById("content").innerHTML = "Nothing to see here!";
+        document.getElementById("main").style.display = "none";
+        document.getElementById("noContent").style.display = "block";
       }
       else {
-        document.getElementById("count").innerHTML = "<span class='title'>Word Count: </span>" + results[0].result.trim().split(/\s+/).length;
-        document.getElementById("readtime").innerHTML = "<span class='title'>Estimated Read Time: </span>" + readingTime(results[0].result) + " mins";
-        document.getElementById("fkcount").innerHTML = "<span class='title'>Flesch-Kincaid Score: </span>" + calculateFleschKincaidReadability(results[0].result);
+        document.getElementById("count").innerHTML = results[0].result.trim().split(/\s+/).length;
+        document.getElementById("readtime").innerHTML = readingTime(results[0].result);
+        displayFKScore(calculateFleschKincaidReadability(results[0].result));
         // Display the text in popup
-        document.getElementById("content").innerHTML = results[0].result;
+        //document.getElementById("content").innerHTML = results[0].result;
       }
     }
   );
